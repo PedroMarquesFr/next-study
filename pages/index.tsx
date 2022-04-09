e9ea44b0291bbfe7716b3c80f0f5027685324b06
Pedro <pedromarques1011@gmail.com>
2022-04-09 18:43:06 -0300

    feat: serve side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import Head from "next/head";
+//server side redering
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+export default function Home({ repositories, date }) {
   return (
-    <div>
-      <Head>
-        <title>HomePage</title>
-      </Head>
-
-      <main>Hello world</main>
-    </div>
+    <section>
+      <h4>{date}</h4>
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo}>{repo}</li>
+        ))}
+      </ul>
+    </section>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch(
+    "https://api.github.com/users/PedroMarquesFr/repos"
+  );
+  const data = await response.json();
+
+  const repositoryNames = data.map((item) => item.name);
+
+  return {
+    props: {
+      repositories: repositoryNames,
+      date: new Date().toISOString(),
+    },
+  };
+};
