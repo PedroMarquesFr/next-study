8ee31035a8875c1c9ef998da577b69c506c41fb6
Pedro <pedromarques1011@gmail.com>
2022-04-09 17:35:13 -0300

    feat: initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+
+      <main>Hello world</main>
+    </div>
+  );
+}
